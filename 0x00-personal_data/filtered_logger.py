63869bab4275ac839fda098c080b5c43a45a7c04
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-09-02 20:31:21 +0000

    added type annotations and a doc string to the filter_datum function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,9 @@ Module containing the filter_datum function
 import re
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
+def filter_datum(fields: list[str],
+                 redaction: str, message: str, separator: str) -> str:
+    """Function performing the redaction on the message"""
     for field in fields:
         pat = r'({}=)([^{}]+{})'.format(field, separator, separator)
         message = re.sub(pat, r'\1{}{}'.format(redaction, separator), message)
