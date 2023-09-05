be045342276af7c8fdd713758fe888b517448238
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-09-05 22:57:57 +0000

    refactored the require_auth to check for routes that do not need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,7 +18,16 @@ class Auth():
         Return:
           - bool
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path in excluded_paths or f"{path}/" in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
