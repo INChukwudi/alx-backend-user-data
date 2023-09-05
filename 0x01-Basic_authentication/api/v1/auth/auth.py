9f24908780cba22ab1e242d535c1177bcd8bf96f
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-09-05 22:24:01 +0000

    added the auth class template in the auth directory

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Auth module
+"""
+
+from flask import request
+
+
+class Auth():
+    """
+    Auth Class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        require auth method
+
+        Return:
+          - bool
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        authorization header method
+
+        Return:
+          - str
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        current user method
+
+        Return:
+          - User object
+        """
+        return None
