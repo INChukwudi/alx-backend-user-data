06f15bc6f6d3d4b39b237091aec464c4685bf7ff
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-09-06 03:31:50 +0000

    updated the authorization_header to check for the authrorization header in the request

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -36,7 +36,13 @@ class Auth():
         Return:
           - str
         """
-        return None
+        if request is None:
+            return None
+
+        if request.headers.get('Authorization') is None:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
