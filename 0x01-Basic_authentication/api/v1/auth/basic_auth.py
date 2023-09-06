d62f2accbfff90df907e6947056769008a6048bd
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-09-06 04:01:15 +0000

    added the method templates in basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,25 @@
 Basic Auth Module
 """
 
+from typing import TypeVar
+
 from .auth import Auth
 
 
 class BasicAuth(Auth):
-    pass
+    """
+    BasicAuth class
+    Inherits from the Auth class
+    """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        return None
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        return (None, None)
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        return None
