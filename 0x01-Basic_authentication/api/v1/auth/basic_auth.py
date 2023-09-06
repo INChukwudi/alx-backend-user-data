a66b2a634f2b5e262eeffd9a61bea361bd215501
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-09-06 04:06:11 +0000

    added documentation for the method templates in basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,13 +15,37 @@ class BasicAuth(Auth):
     """
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        extract_base64_authorization_header
+
+        Return:
+          - str
+        """
         return None
 
     def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        decode_base64_authorization_header
+
+        Return:
+          - str
+        """
         return None
 
     def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        extract_user_credentials
+
+        Return:
+          - (str, str)
+        """
         return (None, None)
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        user_object_from_credentials
+
+        Return:
+          - str
+        """
         return None
