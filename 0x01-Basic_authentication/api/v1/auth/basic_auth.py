30f6fbe4a53c11aeea7d3238537c1af725c5b046
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-09-10 10:45:03 +0000

    fixed styling isseues in basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,7 +14,8 @@ class BasicAuth(Auth):
     Inherits from the Auth class
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """
         extract_base64_authorization_header
 
@@ -23,7 +24,9 @@ class BasicAuth(Auth):
         """
         return None
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
         """
         decode_base64_authorization_header
 
@@ -32,7 +35,9 @@ class BasicAuth(Auth):
         """
         return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
         """
         extract_user_credentials
 
@@ -41,7 +46,9 @@ class BasicAuth(Auth):
         """
         return (None, None)
 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
         """
         user_object_from_credentials
 
