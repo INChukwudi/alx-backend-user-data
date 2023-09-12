280ddadde663335ab701a382e9286c6bbb578a56
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-09-12 17:26:59 +0000

    added the session_auth.py file to git

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Session Auth Module
+"""
+
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth Class definition
+    Inherits from Auth class
+    """
+    pass
